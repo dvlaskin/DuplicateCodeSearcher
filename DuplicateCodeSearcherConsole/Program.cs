0f8861d1d8154908ee8c80a3eb7431adea340889
dvlaskin <dvlaskin.mail@gmail.com>
2021-01-09 20:03:18 +0200

    added method isEnumerableEquals to compare two enumerables. added method TextRowsCompare for compare sources rows

--- DuplicateCodeSearcherConsole/Program.cs
+++ DuplicateCodeSearcherConsole/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DuplicateCodeSearcherLib.Models;
+using DuplicateCodeSearcherLib.Searchers;
 using DuplicateCodeSearcherLib.Utilities;
 
 namespace DuplicateCodeSearcherConsole
@@ -7,17 +11,34 @@ namespace DuplicateCodeSearcherConsole
     {
         static void Main(string[] args)
         {
-            string testStr = "Row1\r\n" +
-                "Row2\r\n" +
-                "\tRow3";
+            var list1 = new List<string>() { "row1", "row2", "row3", "row4", "row5", };
+            var list2 = new List<string>() { "row1", "row2", "row2", "row5", "row5", };
 
-            var textUtil = new TextUtility();
-            var rowList = textUtil.SplitTextToRows(testStr);
+            string text1 = "row1\r\n" +
+                "row2\r\n" +
+                "row3\r\n" +
+                "row4\r\n" +
+                "row5\r\n";
 
-            Console.WriteLine(string.Join("\r\n", rowList));
+            string text2 = "row1.1\r\n" +
+                "row2\r\n" +
+                "row3\r\n" +
+                "row4\r\n";
+
+            var sourceStack = new Stack<ScanSource>();
+            sourceStack.Push(new ScanSource() { Name = "Text1", Text = text1 });
+            sourceStack.Push(new ScanSource() { Name = "Text2", Text = text2 });
+
+            var test = new RowByRowScaner(sourceStack);
+            List<ScanResult> res = test.SearchDuplicates();
+
+
 
             Console.WriteLine("Done!");
+            
             Console.ReadKey();
+            Console.Clear();
         }
+
     }
 }
--- DuplicateCodeSearcherLib/Searchers/RowByRowScaner.cs
+++ DuplicateCodeSearcherLib/Searchers/RowByRowScaner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DuplicateCodeSearcherLib.Models;
 using DuplicateCodeSearcherLib.Utilities;
 
@@ -20,7 +21,72 @@ namespace DuplicateCodeSearcherLib.Searchers
             List<string> rowsList = textUtil.SplitTextToRows(currText.Text);
 
 
+            foreach (var item in _textsForScan)
+            {
+                var itemList = textUtil.SplitTextToRows(item.Text);
+                TextRowsCompare(rowsList, itemList);
+            }
+
+
             return result;
+        }
+
+
+        private void TextRowsCompare(IEnumerable<string> mainRows, IEnumerable<string> comparableRows)
+        {
+            int rowCount = mainRows.Count() < comparableRows.Count() ? mainRows.Count() : comparableRows.Count();
+
+            for (int i = rowCount; i > 1; i--)
+            {
+                Console.WriteLine($"{i}==========================================");
+
+                for (int l = 0; l < rowCount; l++)
+                {
+                    if ((i + l) > rowCount)
+                        break;
+
+                    Console.WriteLine("mRows: " + string.Join(", ", mainRows.Skip(l).Take(i)));
+                    Console.WriteLine("cRows: " + string.Join(", ", comparableRows.Skip(l).Take(i)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Comparing two Enumerable for equality
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source1"></param>
+        /// <param name="source2"></param>
+        /// <returns></returns>
+        private bool isEnumerableEquals<T>(IEnumerable<T> source1, IEnumerable<T> source2)
+        {
+            var cnt = new Dictionary<T, int>();
+
+            foreach (T s in source1)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]++;
+                }
+                else
+                {
+                    cnt.Add(s, 1);
+                }
+            }
+
+            foreach (T s in source2)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return cnt.Values.All(c => c == 0);
         }
     }
 }
