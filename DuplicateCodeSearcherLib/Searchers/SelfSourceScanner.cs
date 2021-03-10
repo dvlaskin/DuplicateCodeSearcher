e40b6a3777e283835749397afac3e99c442a8dbd
dvlaskin <dvlaskin.mail@gmail.com>
2021-03-10 21:21:47 +0200

    CodeReview class SelfSourceScanner

--- DuplicateCodeSearcherLib/Searchers/SelfSourceScanner.cs
+++ DuplicateCodeSearcherLib/Searchers/SelfSourceScanner.cs
@@ -6,96 +6,139 @@ using DuplicateCodeSearcherLib.Utilities;
 
 namespace DuplicateCodeSearcherLib.Searchers
 {
+    /// <summary>
+    /// Sanner duplicate rows in self text
+    /// </summary>
     public class SelfSourceScanner : IScanSelfText
     {
+        private int _mainIndexRowPosition;
+        private List<string> _rowsList;
+        private StringBuilder _duplCodeText;
+
+        /// <summary>
+        /// Scan and find duplicate rows in self text
+        /// </summary>
+        /// <param name="rowsList">List rows to scan</param>
+        /// <returns></returns>
         public Dictionary<string, int> Scan(List<string> rowsList)
         {
             var result = new Dictionary<string, int>();
 
-            int indexMainRow = 0;
+            _mainIndexRowPosition = 0;
+            _rowsList = rowsList;            
 
-            while (indexMainRow < rowsList.Count)
+            while (_mainIndexRowPosition < _rowsList.Count)
             {
-                string currRow = rowsList[indexMainRow];
+                string currRow = _rowsList[_mainIndexRowPosition];
 
                 // находим начальные индексы дубл. строки
-                List<List<int>> duplRowIndxs = rowsList.FindAllIndexesOf(currRow, indexMainRow + 1);
-
-                // признак наличия дублика проверяемой строки
-                bool duplRowsExists = duplRowIndxs.Count > 0;
+                List<List<int>> duplRowIndxs = FindStartDuplIndexs(currRow);
 
                 // если дубликатов нет, переходим к следующей строке
-                if (duplRowsExists == false)
+                if (duplRowIndxs.Count == 0)
                 {
-                    indexMainRow++;
+                    _mainIndexRowPosition++;
                     continue;
                 }
 
-                var duplCodeText = new StringBuilder();
-                duplCodeText.AppendLine(currRow);
+                _duplCodeText = new StringBuilder();
+                _duplCodeText.AppendLine(currRow);          
+                
+                // пошаговая проверка начальных дубл. строк
+                CheckDuplicateNextRows(duplRowIndxs);
 
-                // индекс седующей строки для сравнения на дубликат
-                int nextRowStep = 1;
-
-                // пока есть дубликат, проверяем следующую строку оригинала
-                // со следующей строкой дубликата
-                while (duplRowsExists)
+                if (_duplCodeText.Length > 1)
                 {
-                    duplRowsExists = false;
+                    result.Add(_duplCodeText.ToString(), duplRowIndxs.Count);
 
-                    // производим сравнение по всем найденым строкам
-                    // начальной строки оригинала
-                    for (int i = 0; i < duplRowIndxs.Count; i++)
-                    {
-                        // индекс начальной строки дублирующегося кода
-                        int duplRowStartIndex = duplRowIndxs[i][0];
-
-                        // проверяем что бы следующая проверяемая строка
-                        // не выходила за границы массива строк
-                        if (duplRowStartIndex + nextRowStep >= rowsList.Count)
-                            break;
-
-                        // сравниваем следущую строку оригинала
-                        // со следующей строкой дубликата
-                        if (rowsList[indexMainRow + nextRowStep] == rowsList[duplRowStartIndex + nextRowStep])
-                        {
-                            // если строки совпадают
-                            // добавляем в перечень строк дубликата
-                            duplRowsExists = true;
-                            duplRowIndxs[i].Add(duplRowStartIndex + nextRowStep);                            
-                        }
-                    }
-
-                    // если было найдено совпадение
-                    // по сравнению следующих строк
-                    if (duplRowsExists)
-                    {
-                        // удаляем ранее найденые дубликаты, которые
-                        // по количеству строк меньше, чем последние найденые
-                        duplRowIndxs.RemoveAll(w => w.Count() <= nextRowStep);
-                        duplCodeText.AppendLine(rowsList[indexMainRow + nextRowStep]);
-                        nextRowStep++;
-                    }
+                    RemoveDuplicateFromRowsList(duplRowIndxs);
                 }
-
-                if (duplCodeText.Length > 1)
-                {
-                    result.Add(duplCodeText.ToString(), duplRowIndxs.Count);
-                    foreach (var duplItem in duplRowIndxs)
-                    {
-                        rowsList.RemoveRange(duplItem[0], duplItem.Count);
-                    }
-                }
-
-
-                // увеличиваем шаг посточного сравнения
-                // на значение последней просмотренной строки
-                indexMainRow += nextRowStep;
             }
 
             //Console.WriteLine($"selfIterationCounter = {currIterationCounter}");
 
             return result;
+        }
+
+
+        /// <summary>
+        /// Checking duplicate string sequences to match the original string sequence
+        /// </summary>
+        /// <param name="duplRowIndxs">A start indexes duplicate sequence</param>
+        private void CheckDuplicateNextRows(List<List<int>> duplRowIndxs)
+        {
+            bool duplRowsExists = true;
+            // индекс седующей строки для сравнения на дубликат
+            int nextRowStep = 1;
+
+            // пока есть дубликат, проверяем следующую строку оригинала
+            // со следующей строкой дубликата
+            while (duplRowsExists)
+            {
+                duplRowsExists = false;
+
+                // производим сравнение по всем найденым строкам
+                // начальной строки оригинала
+                for (int i = 0; i < duplRowIndxs.Count; i++)
+                {
+                    // индекс начальной строки дублирующегося кода
+                    int duplRowStartIndex = duplRowIndxs[i][0];
+
+                    // проверяем чтобы следующая проверяемая строка
+                    // не выходила за границы массива строк
+                    if (duplRowStartIndex + nextRowStep >= _rowsList.Count)
+                        break;
+
+                    // сравниваем следущую строку оригинала
+                    // со следующей строкой дубликата
+                    if (_rowsList[_mainIndexRowPosition + nextRowStep] == _rowsList[duplRowStartIndex + nextRowStep])
+                    {
+                        // если строки совпадают
+                        // добавляем в перечень строк дубликата
+                        duplRowsExists = true;
+                        duplRowIndxs[i].Add(duplRowStartIndex + nextRowStep);
+                    }
+                }
+
+                // если было найдено совпадение
+                // по сравнению следующих строк
+                if (duplRowsExists)
+                {
+                    // удаляем ранее найденые дубликаты, которые
+                    // по количеству строк меньше, чем последние найденые
+                    duplRowIndxs.RemoveAll(w => w.Count() <= nextRowStep);
+                    _duplCodeText.AppendLine(_rowsList[_mainIndexRowPosition + nextRowStep]);
+                    nextRowStep++;
+                }
+            }
+
+
+            // увеличиваем шаг посточного сравнения
+            // на значение последней просмотренной строки
+            _mainIndexRowPosition += nextRowStep;
+        }
+
+        /// <summary>
+        /// Remove founded duplicate lines from the full list of lines
+        /// </summary>
+        /// <param name="duplRowIndxs">Duplicate rows indexes</param>
+        private void RemoveDuplicateFromRowsList(List<List<int>> duplRowIndxs)
+        {
+            for (int i = duplRowIndxs.Count - 1; i >= 0; i--)
+            {
+                _rowsList.RemoveRange(duplRowIndxs[i][0], duplRowIndxs[i].Count);
+            }
+        }
+
+        /// <summary>
+        /// Search duplicate of current row in self text
+        /// </summary>
+        /// <param name="currRow"></param>
+        /// <returns></returns>
+        private List<List<int>> FindStartDuplIndexs(string currRow)
+        {
+            return _rowsList
+                .FindAllIndexesOf(currRow, _mainIndexRowPosition + 1);
         }
     }
 }
